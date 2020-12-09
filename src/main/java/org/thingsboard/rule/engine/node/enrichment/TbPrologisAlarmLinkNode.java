bf47177666e0dfc1e53cb985e92157b717277fb5
ikrivoruchko <ikrivoruchko@thingsboard.io>
2020-12-09 19:00:35 +0200

    created node TbPrologisAlarmLinkNode

---
+++ src/main/java/org/thingsboard/rule/engine/node/enrichment/TbPrologisAlarmLinkNode.java
@@ -0,0 +1,160 @@
+package org.thingsboard.rule.engine.node.enrichment;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
+import org.thingsboard.common.util.DonAsynchron;
+import org.thingsboard.rule.engine.api.*;
+import org.thingsboard.rule.engine.api.util.TbNodeUtils;
+import org.thingsboard.server.common.data.EntityType;
+import org.thingsboard.server.common.data.asset.Asset;
+import org.thingsboard.server.common.data.id.*;
+import org.thingsboard.server.common.data.page.PageLink;
+import org.thingsboard.server.common.data.plugin.ComponentType;
+import org.thingsboard.server.common.data.relation.*;
+import org.thingsboard.server.common.msg.TbMsg;
+
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+@RuleNode(
+        type = ComponentType.ENRICHMENT,
+        name = "prologis alarm link",
+        configClazz = EmptyNodeConfiguration.class,
+        nodeDescription = "",
+        nodeDetails = "",
+        configDirective = "tbNodeEmptyConfig")
+public class TbPrologisAlarmLinkNode implements TbNode {
+    private static final String DASHBOARD_NAME = "Pump Rooms";
+    private static final String LINK_PREFIX = "http://localhost:8080/dashboards/";
+    private static final String STATE = "/?state=";
+    private static final String ID_BUILDINGS = "buildings";
+    private static final String ID_BUILDING_INDICATORS = "building_indicators_and_card_with_move_count";
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private EmptyNodeConfiguration config;
+    private DashboardId dashboardId;
+    @Override
+    public void init(TbContext ctx, TbNodeConfiguration configuration) throws TbNodeException {
+        config = TbNodeUtils.convert(configuration, EmptyNodeConfiguration.class);
+        Optional<DashboardId> dashboardIdOptional = ctx.getDashboardService()
+                .findDashboardsByTenantId(ctx.getTenantId(), new PageLink(1000))
+                .getData()
+                .stream()
+                .filter(dashboardInfo -> dashboardInfo.getName().equals(DASHBOARD_NAME))
+                .map(IdBased::getId)
+                .findFirst();
+        if (dashboardIdOptional.isPresent()) {
+            dashboardId = dashboardIdOptional.get();
+        } else {
+            log.warn("Didn't find dashboard with name = {}", DASHBOARD_NAME);
+        }
+    }
+
+    @Override
+    public void onMsg(TbContext ctx, TbMsg msg) throws ExecutionException, InterruptedException, TbNodeException {
+        DonAsynchron.withCallback(getArrayNode(ctx, msg), arrayNode -> {
+            if (arrayNode != null) {
+                try {
+                    byte[] bytes = OBJECT_MAPPER.writeValueAsBytes(arrayNode);
+                    String encodeToString = Base64.getEncoder().encodeToString(bytes);
+                    String alarmLink = LINK_PREFIX +
+                            dashboardId.getId() +
+                            STATE +
+                            encodeToString;
+                    msg.getMetaData().putValue("alarmLink", alarmLink);
+                } catch (JsonProcessingException e) {
+                    log.warn("Didn't parse json to bytes, error = []", e);
+                }
+            }
+            ctx.tellSuccess(msg);
+        }, throwable -> ctx.tellFailure(msg, throwable));
+
+    }
+
+    private ListenableFuture<ArrayNode> getArrayNode(TbContext ctx, TbMsg msg) {
+        ObjectNode firstObject = OBJECT_MAPPER.createObjectNode();
+        firstObject.put("id", ID_BUILDINGS);
+        firstObject.set("params", OBJECT_MAPPER.createObjectNode());
+
+        ObjectNode secondObject = OBJECT_MAPPER.createObjectNode();
+        secondObject.put("id", ID_BUILDING_INDICATORS);
+
+        return Futures.transform(getParams(ctx, msg), params -> {
+            if (params == null) {
+                return null;
+            }
+            secondObject.set("params", params);
+            ArrayNode arrayNode = OBJECT_MAPPER.createArrayNode();
+            arrayNode.add(firstObject);
+            arrayNode.add(secondObject);
+            return arrayNode;
+        },ctx.getDbCallbackExecutor());
+    }
+
+    private ListenableFuture<ObjectNode> getParams(TbContext ctx, TbMsg msg) {
+        String alarmId;
+        try {
+            JsonNode msgDataNode = OBJECT_MAPPER.readTree(msg.getData());
+            if (msgDataNode.has("id") && msgDataNode.get("id").has("id")) {
+                alarmId = msgDataNode.get("id").get("id").asText();
+            } else {
+                log.warn("Didn't find alarm id in msg = {}", msg);
+                return Futures.immediateFuture(null);
+            }
+        } catch (JsonProcessingException e) {
+            log.warn("Didn't parse data msg = {}- to json node", msg);
+            return Futures.immediateFuture(null);
+        }
+        return Futures.transform(getBuilding(ctx, msg.getOriginator()), building -> {
+            if (building != null) {
+                ObjectNode entityId = OBJECT_MAPPER.createObjectNode();
+                entityId.put("entityType", "ASSET");
+                entityId.put("id", building.getId().toString());
+
+                ObjectNode params = OBJECT_MAPPER.createObjectNode();
+                params.set("entityId", entityId);
+                params.put("entityName", building.getName());
+                params.put("entityLabel", building.getName());
+                params.put("alarmId", alarmId);
+                return params;
+            } else {
+                log.warn("Didn't find building for device with id = {}", msg.getOriginator());
+            }
+         return null;
+        }, ctx.getDbCallbackExecutor());
+    }
+
+    private ListenableFuture<Asset> getBuilding(TbContext ctx, EntityId originator) {
+        ListenableFuture<List<EntityRelation>> entityRelationsFuture = ctx.getRelationService()
+                .findByQuery(ctx.getTenantId(), getEntityRelationsQuery(originator));
+        return Futures.transform(entityRelationsFuture, entityRelations -> {
+            if (!CollectionUtils.isEmpty(entityRelations)) {
+                EntityId buildingId = entityRelations.get(0).getFrom();
+                return ctx.getAssetService().findAssetById(ctx.getTenantId(), new AssetId(buildingId.getId()));
+            }
+            return null;
+        },ctx.getDbCallbackExecutor());
+    }
+
+    private EntityRelationsQuery getEntityRelationsQuery(EntityId originatorId) {
+        RelationsSearchParameters relationsSearchParameters = new RelationsSearchParameters(originatorId,
+                EntitySearchDirection.TO, 5, false);
+        EntityTypeFilter entityTypeFilter = new EntityTypeFilter("IS_IN_PROJECT", Collections.singletonList(EntityType.ASSET));
+        EntityRelationsQuery entityRelationsQuery = new EntityRelationsQuery();
+        entityRelationsQuery.setParameters(relationsSearchParameters);
+        entityRelationsQuery.setFilters(Collections.singletonList(entityTypeFilter));
+        return entityRelationsQuery;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
